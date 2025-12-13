ac1d2edf988eb44a7b8f5f14b7241674386b9180
Roberto Gonzalez <jrgonzalez.lsca@gmail.com>
2025-12-13 01:15:35 -0600

    Complete sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'feed#show'
+
+get "sign_up", to: "users#new"
+post "sign_up", to: "users#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
